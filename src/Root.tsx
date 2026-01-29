ec2ec0a4325774800bbcfe2443544ac0f38ed95f
Александр Лысак <frigh701@gmail.com>
2026-01-29 10:08:31 +0300

    include all variants of code display to root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,26 +1,17 @@
 import "./index.css";
 import { Composition } from "remotion";
 import { CodeDisplay } from "./CodeDisplay";
-
-// Sample code for demo
-const sampleCode = `const greeting = "Hello, World!";
-
-function sayHello(name: string) {
-  console.log(\`Hello, \${name}!\`);
-  return name.toUpperCase();
-}
-
-// Call the function
-sayHello(greeting);`;
+import { CodeDisplayDemo } from "./CodeDisplay/Demo";
+import { CodeDisplayDemoPage } from "./CodeDisplay/DemoPage";
+import { sampleCode, codeSamples } from "./CodeDisplay/codeLoader";
 
 // Each <Composition> is an entry in the sidebar!
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* Basic CodeDisplay - TypeScript sample */}
       <Composition
-        // You can take the "id" to render a video:
-        // npx remotion render CodeDisplay
         id="CodeDisplay"
         component={CodeDisplay}
         durationInFrames={300}
@@ -32,6 +23,68 @@ export const RemotionRoot: React.FC = () => {
           filename: "example.tsx",
           showCursor: true,
         }}
+      />
+
+      {/* TypeScript Demo */}
+      <Composition
+        id="CodeDisplayTS"
+        component={CodeDisplayDemo}
+        durationInFrames={450}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{
+          codeType: "typescript" as keyof typeof codeSamples,
+        }}
+      />
+
+      {/* Python Demo */}
+      <Composition
+        id="CodeDisplayPython"
+        component={CodeDisplayDemo}
+        durationInFrames={400}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{
+          codeType: "python" as keyof typeof codeSamples,
+        }}
+      />
+
+      {/* JavaScript Demo */}
+      <Composition
+        id="CodeDisplayJS"
+        component={CodeDisplayDemo}
+        durationInFrames={400}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{
+          codeType: "javascript" as keyof typeof codeSamples,
+        }}
+      />
+
+      {/* CSS Demo */}
+      <Composition
+        id="CodeDisplayCSS"
+        component={CodeDisplayDemo}
+        durationInFrames={350}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{
+          codeType: "css" as keyof typeof codeSamples,
+        }}
+      />
+
+      {/* Full Demo Page with all scenes */}
+      <Composition
+        id="CodeDisplayFullDemo"
+        component={CodeDisplayDemoPage}
+        durationInFrames={1290}
+        fps={30}
+        width={1920}
+        height={1080}
       />
     </>
   );
